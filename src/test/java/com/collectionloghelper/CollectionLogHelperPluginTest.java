60428c5e0ba665d26f67f9b7a1b4a02a396ba9b6
cha-ndler <48898494+cha-ndler@users.noreply.github.com>
2026-03-16 12:21:11 -0400

    Fix deprecation and unchecked compilation warnings
    - Replace deprecated InventoryID enum with gameval.InventoryID constants
    - Replace deprecated Skill.OVERALL with null check
    - Add @SuppressWarnings for unavoidable generic array creation in test

--- src/test/java/com/collectionloghelper/CollectionLogHelperPluginTest.java
+++ src/test/java/com/collectionloghelper/CollectionLogHelperPluginTest.java
@@ -5,6 +5,7 @@ import net.runelite.client.externalplugins.ExternalPluginManager;
 
 public class CollectionLogHelperPluginTest
 {
+	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws Exception
 	{
 		System.setProperty("collectionlog.devmode", "true");
