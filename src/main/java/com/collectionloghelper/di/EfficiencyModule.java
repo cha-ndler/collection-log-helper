1e0d4599c5fd3bdbf9c784042adc6eaddfa53cc1
cha-ndler <48898494+cha-ndler@users.noreply.github.com>
2026-05-18 10:49:13 -0400

    refactor: extract EfficiencyModule (#504) (#518)
    Consolidate the five efficiency/analytics-layer @Inject fields into a
    single EfficiencyModule aggregate, mirroring DataModule (#509) and
    GuidanceModule (#514).
    
    Fields extracted:
    - EfficiencyCalculator calculator
    - ClueCompletionEstimator clueEstimator
    - SlayerStrategyCalculator slayerStrategyCalculator
    - DryStreakAnalyzer dryStreakAnalyzer
    - KillTimeTracker killTimeTracker
    
    CollectionLogHelperPlugin @Inject count: 25 -> 21.
    
    Third of four planned sub-module extractions tracked by #504.
    
    Partially addresses #504

---
+++ src/main/java/com/collectionloghelper/di/EfficiencyModule.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2025, cha-ndler
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.collectionloghelper.di;
+
+import com.collectionloghelper.efficiency.ClueCompletionEstimator;
+import com.collectionloghelper.efficiency.EfficiencyCalculator;
+import com.collectionloghelper.efficiency.SlayerStrategyCalculator;
+import com.collectionloghelper.learning.DryStreakAnalyzer;
+import com.collectionloghelper.learning.KillTimeTracker;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import lombok.Getter;
+
+/**
+ * Aggregate holder for the plugin's efficiency-layer Guice singletons.
+ *
+ * <p>Groups the five efficiency/analytics dependencies that {@code CollectionLogHelperPlugin}
+ * previously injected individually, reducing the plugin's {@code @Inject} field count
+ * and providing a single seam for efficiency-layer wiring.
+ *
+ * <p>Third of four planned sub-module extractions tracked by issue #504.
+ */
+@Singleton
+@Getter
+public class EfficiencyModule
+{
+	private final EfficiencyCalculator calculator;
+	private final ClueCompletionEstimator clueEstimator;
+	private final SlayerStrategyCalculator slayerStrategyCalculator;
+	private final DryStreakAnalyzer dryStreakAnalyzer;
+	private final KillTimeTracker killTimeTracker;
+
+	@Inject
+	public EfficiencyModule(
+		EfficiencyCalculator calculator,
+		ClueCompletionEstimator clueEstimator,
+		SlayerStrategyCalculator slayerStrategyCalculator,
+		DryStreakAnalyzer dryStreakAnalyzer,
+		KillTimeTracker killTimeTracker)
+	{
+		this.calculator = calculator;
+		this.clueEstimator = clueEstimator;
+		this.slayerStrategyCalculator = slayerStrategyCalculator;
+		this.dryStreakAnalyzer = dryStreakAnalyzer;
+		this.killTimeTracker = killTimeTracker;
+	}
+}
